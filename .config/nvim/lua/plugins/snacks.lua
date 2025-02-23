a9bb37de75e8748cfaf64df16115e2bc8ad45bf9
Matthew Carbone <x94carbone@gmail.com>
2025-02-23 11:10:43 -0500

    Cleanup snacks.lua
    Remove a few lines and comments from snacks.lua

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,13 +1,12 @@
 -- lazy.nvim
 return {
     "folke/snacks.nvim",
+    lazy = false,
     ---@type snacks.Config
     opts = {
         picker = {
+            enabled = true,
             hidden = true,
-            -- your picker configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
         },
     },
 }

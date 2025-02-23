22b9d5b77eb445b49ef5900521621d9fa408e6c0
Matthew Carbone <x94carbone@gmail.com>
2025-02-23 10:11:48 -0500

    Replace nvim-cmp completely with blink

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,26 +1,19 @@
+-- Controls the dropdown autocompletes
 return {
     "saghen/blink.cmp",
-    lazy = false, -- lazy loading handled internally
-    -- optional: provides snippets for the snippet source
-    dependencies = "rafamadriz/friendly-snippets",
-
-    -- use a release tag to download pre-built binaries
-    version = "v0.*",
-    -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
-
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
     opts = {
-        -- 'default' for mappings similar to built-in completion
-        -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-        -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-        -- see the "default configuration" section below for full documentation on how to define
-        -- your own keymap.
+        sources = {
+            default = { "lazydev" },
+            providers = {
+                lazydev = {
+                    name = "LazyDev",
+                    module = "lazydev.integrations.blink",
+                    score_offset = 100, -- show at a higher priority than lsp
+                },
+            },
+        },
         keymap = {
-            -- preset = "default",
+            preset = "enter",
             ["<C-d>"] = {
                 "show",
                 "show_documentation",
@@ -29,95 +22,5 @@ return {
             ["<C-k>"] = { "select_prev", "fallback" },
             ["<C-j>"] = { "select_next", "fallback" },
         },
-        --   ['<C-e>'] = { 'hide' },
-        --   ['<C-y>'] = { 'select_and_accept' },
-        --
-        --   ['<C-p>'] = { 'select_prev', 'fallback' },
-        --   ['<C-n>'] = { 'select_next', 'fallback' },
-        --
-        --   ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-        --   ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-        --
-        --   ['<Tab>'] = { 'snippet_forward', 'fallback' },
-        --   ['<S-Tab>'] = { 'snippet_backward', 'fallback' },},
-
-        appearance = {
-            -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-            -- Useful for when your theme doesn't support blink.cmp
-            -- will be removed in a future release
-            use_nvim_cmp_as_default = true,
-            -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-            -- Adjusts spacing to ensure icons are aligned
-            nerd_font_variant = "mono",
-        },
-
-        -- default list of enabled providers defined so that you can extend it
-        -- elsewhere in your config, without redefining it, via `opts_extend`
-        sources = {
-            default = { "lsp", "path", "snippets", "buffer" },
-            -- optionally disable cmdline completions
-            -- cmdline = {},
-        },
-
-        -- experimental signature help support
-        -- signature = { enabled = true }
     },
-    -- allows extending the providers array elsewhere in your config
-    -- without having to redefine it
-    opts_extend = { "sources.default" },
-
-    config = function()
-        local blink = require("blink.cmp")
-
-        blink.setup({
-            completion = {
-                menu = {
-                    draw = {
-                        components = {
-                            -- customize the drawing of kind icons
-                            kind_icon = {
-                                text = function(ctx)
-                                    -- default kind icon
-                                    local icon = ctx.kind_icon
-                                    -- if LSP source, check for color derived from documentation
-                                    if ctx.item.source_name == "LSP" then
-                                        local color_item = require(
-                                            "nvim-highlight-colors"
-                                        ).format(
-                                            ctx.item.documentation,
-                                            { kind = ctx.kind }
-                                        )
-                                        if color_item and color_item.abbr then
-                                            icon = color_item.abbr
-                                        end
-                                    end
-                                    return icon .. ctx.icon_gap
-                                end,
-                                highlight = function(ctx)
-                                    -- default highlight group
-                                    local highlight = "BlinkCmpKind" .. ctx.kind
-                                    -- if LSP source, check for color derived from documentation
-                                    if ctx.item.source_name == "LSP" then
-                                        local color_item = require(
-                                            "nvim-highlight-colors"
-                                        ).format(
-                                            ctx.item.documentation,
-                                            { kind = ctx.kind }
-                                        )
-                                        if
-                                            color_item
-                                            and color_item.abbr_hl_group
-                                        then
-                                            highlight = color_item.abbr_hl_group
-                                        end
-                                    end
-                                    return highlight
-                                end,
-                            },
-                        },
-                    },
-                },
-            },
-        })
-    end,
 }
